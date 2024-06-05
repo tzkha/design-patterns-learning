542e3e38b2cf3588a189d1a972afdf9a37519d3a
tzk_cam <1364819787@qq.com>
2024-06-05 00:29:12 -0700

    thread

---
+++ 创建型模式/PS/getid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void func(int num, string str){
+    for(int i = 0; i < 10; i++){
+        cout<< "son thread i = "<< i << " num: "<< num << " str: "<< str << endl;  
+    }
+}
+
+void func1(){
+    for(int i = 0; i < 10; i++){
+        cout<< "son thread i = "<< i << endl;  
+    }
+}
+
+int main(){
+    cout<< "main thread id: "<< this_thread::get_id() << endl;
+    thread t(func, 520, "i love you");
+    thread t1(func1);
+    cout << "t id : "<< t.get_id() << endl;
+    cout << "t1 id : "<< t1.get_id() << endl;
+    t.join();
+    t1.join();
+}
